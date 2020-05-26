bad13e33f9f1c3cdb124403c4b5a215fd8f7b396
Lauren Gillespie <leg2015@gmail.com>
2020-05-26 15:08:53 -0700

    got dataloader working. Reading files still incredibly slow on memex and calc so more optimizing will need to be done

--- deepbiosphere/scripts/GEOCELF_dataloader.py
+++ deepbiosphere/scripts/GEOCELF_dataloader.py
@@ -1,10 +1,10 @@
 from __future__ import print_function, division
-import os
+#import os
 import torch
 import pandas as pd
 import numpy as np
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
+from torch.utils.data import Dataset
+#from torchvision import transforms, utils
 
 # Ignore warnings
 import warnings
@@ -16,14 +16,14 @@ def get_gbif_data(pth, split, country):
     obs_pth = f"{pth}occurrences/occurrences_{country}_{split}.csv"
     return pd.read_csv(obs_pth, sep=';')  
 
-def us_image_from_id(id_, pth):
+def us_image_from_id(id_, pth, country):
     abcd = id_ % 10000
     ab, cd = math.floor(abcd/100), abcd%100
     cdd = math.ceil((cd+ 1)/5)
     cdd = f"0{cdd}"  if cdd < 10 else f"{cdd}"
     ab = f"0{ab}" if id_ / 1000 > 1 and ab < 10 else ab
-    cd = f"0{cd}" if id_ / 1000 > 1 and cd < 10 else cd
-    subpath = f"patches_us_{cdd}/{cd}/{ab}/"
+    cd = f"0{cd}" if cd < 10 else cd
+    subpath = f"patches_{country}/patches_{country}_{cdd}/{cd}/{ab}/"
     alt = f"{pth}{subpath}{id_}_alti.npy"
     rgbd = f"{pth}{subpath}{id_}.npy"    
     np_al = np.load(alt)
@@ -35,36 +35,25 @@ def us_image_from_id(id_, pth):
 def fr_img_from_id(id_, pth):
     raise NotImplementedError
 
-def us_image_from_id(id_, pth):
-    abcd = id_ % 10000
-    ab, cd = math.floor(abcd/100), abcd%100
-    cdd = math.ceil((cd+ 1)/5)
-    cdd = f"0{cdd}"  if cdd < 10 else f"{cdd}"
-    ab = f"0{ab}" if id_ / 1000 > 1 and ab < 10 else ab
-    cd = f"0{cd}" if id_ / 1000 > 1 and cd < 10 else cd
-    subpath = f"patches_us_{cdd}/{cd}/{ab}/"
-    alt = f"{pth}{subpath}{id_}_alti.npy"
-    rgbd = f"{pth}{subpath}{id_}.npy"    
-    np_al = np.load(alt)
-    np_img = np.load(rgbd)
-    np_al = np.expand_dims(np_al, 2)
-    np_all = np.concatenate((np_al, np_img), axis=2)
-    return np_all
 
 def prep_US_data(us_obs):
     spec_2_id = {k:v for k, v in zip(us_obs.species_id.unique(), np.arange(len(us_obs.species_id.unique())))}
     us_obs['species_id'] = us_obs['species_id'].map(spec_2_id)
 
-class GeoClefDataset(Dataset):
-    def __init__(self, obs_path, split='train', country='us', img_dir, transform=None):
+class GEOCELF_Dataset(Dataset):
+    def __init__(self, base_dir, split='train', country='us', transform=None):
         
-        obs = get_gbif_data(obs_path, split, country)
-        if country == 'us':
+        self.base_dir = base_dir
+        self.country = country
+        self.split = split
+        obs = get_gbif_data(self.base_dir, split, country)
+        if self.country == 'us':
             obs = prep_US_data(obs)
         # Grab only obs id, species id because lat /lon not necessary at the moment
-        self.obs = us_obs[['id', 'species_id']].to_numpy()
-        self.img_dir = img_dir
+        self.num_cats = len(obs.species_id.unique())
+        self.obs = obs[['id', 'species_id']].to_numpy()
         self.transform = transform
+        self.channels = us_image_from_id(obs[0,0]).shape[0]
 
     def __len__(self):
         return len(self.obs)
@@ -73,10 +62,12 @@ class GeoClefDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         id_, label = self.obs[idx, 0], self.obs[idx, 1]
-        images = us_image_from_id(id_, self.img_dir) 
+        images = us_image_from_id(id_, self.base_dir, self.country) 
         if self.transform:
             images = self.transform(images)
         return (label, images)
 
-
-
+    def num_cats(self):
+        return self.num_cats
+    def num_channels(self):
+        return self.channels
--- deepbiosphere/scripts/GEOCELF_us_cnn_train_dataloader.py
+++ deepbiosphere/scripts/GEOCELF_us_cnn_train_dataloader.py
@@ -1,16 +1,20 @@
+from random import randrange
 import pandas as pd
 import argparse
 import time
 import numpy as np
-
+import socket
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
 import random
 import math
 from tqdm import tqdm
 from deepbiosphere.scripts import GEOCELF_CNN as cnn
+from deepbiosphere.scripts import GEOCELF_Dataset as Dataset
 from deepbiosphere.scripts import paths
         
 # https://gist.github.com/weiaicunzai/2a5ae6eac6712c70bde0630f3e76b77b        
@@ -28,106 +32,39 @@ def topk_acc(output, target, topk=(1,), device=None):
     del targ
     return res
 
-def us_image_from_id(id_, pth):
-    abcd = id_ % 10000
-    ab, cd = math.floor(abcd/100), abcd%100
-    cdd = math.ceil((cd+ 1)/5)
-    cdd = f"0{cdd}"  if cdd < 10 else f"{cdd}"
-    ab = f"0{ab}" if id_ / 1000 > 1 and ab < 10 else ab
-    cd = f"0{cd}" if id_ / 1000 > 1 and cd < 10 else cd
-    subpath = f"patches_us_{cdd}/{cd}/{ab}/"
-    alt = f"{pth}{subpath}{id_}_alti.npy"
-    rgbd = f"{pth}{subpath}{id_}.npy"    
-    np_al = np.load(alt)
-    np_img = np.load(rgbd)
-    np_al = np.expand_dims(np_al, 2)
-    np_all = np.concatenate((np_al, np_img), axis=2)
-    return np_all
-
-def tensor_from_ids(ids, img_pth, device):
-    x_tens = [us_image_from_id(id_, img_pth) for id_ in ids]
-    x_tens = torch.from_numpy(np.stack(x_tens)) #should be size 256 x 256 x 6 x batchsize
-    x_tens = x_tens.permute(0,3,1,2)# reshape to shape torch expects: ((N,Cin,H,W))
-    return x_tens
-
-def get_gbif_data(pth, country):
-    ## Grab GBIF observation data
-    train_pth = f"{pth}occurrences/occurrences_{country}_train.csv"
-    test_pth = f"{pth}occurrences/occurrences_{country}_test.csv"
-    train = pd.read_csv(train_pth, sep=';')
-    test = pd.read_csv(test_pth, sep=';')  
-    return train, test
-  
-    
 def split_train_test(full_dat, split_amt):
     #grab 20% of labeled data for holdout testing
     idxs = np.random.permutation(len(full_dat))
     split = int(len(idxs)*split_amt)
     training_idx, test_idx = idxs[:split], idxs[split:]
-    training, test = full_dat[training_idx,:], full_dat[test_idx,:]  
-    assert not np.array_equal(training[:20], full_dat[:20]), "permutation didn't work!"
-    return training, test
-
-def split_id_specs(obs_data, device):
-    obs_ids, obs_spec_ids = obs_data[:,0], obs_data[:,1]
-    obs_spec_ids = torch.from_numpy(obs_spec_ids)
-    obs_spec_ids = obs_spec_ids
-    return obs_ids, obs_spec_ids
-
-def shuffle_data(obs, labels):
-    indxs = torch.randperm(len(labels))
-    return  obs[indxs], labels[indxs]
-    
-
-def batch_data(obs, labels, batch_size):
-#     assert training_imgs.shape[0] == len(train_ids), "number of training examples and labels don't match!"
-    curr_obs, curr_labels = shuffle_data(obs, labels)
-    label_bat = torch.split(curr_labels, batch_size)
-    obs_bat = torch.split(curr_obs, batch_size)
-    return obs_bat, label_bat
+    train_sampler = SubsetRandomSampler(training_idx)
+    valid_sampler = SubsetRandomSampler(test_idx)
+    return train_sampler, valid_sampler
 
 def main():
     
     device = torch.device(f"cuda:{ARGS.device}" if ARGS.device is not None else "cpu")
     print(f'using device: {device}')
-    
+    if ARGS.device is not None:
+        print(f"current device: {torch.cuda.current_device()} current device name: {torch.cuda.get_device_name(torch.cuda.current_device())}")
+    print(f"current host: {socket.gethostname()}")
+
     # load observation data
-    print("loading labels")
-    pth = paths.GEOCELF_DIR
-
-    us_train, _ = get_gbif_data(pth, 'us')
-    spec_2_id = {k:v for k, v in zip(us_train.species_id.unique(), np.arange(len(us_train.species_id.unique())))}
-    us_train['species_id'] = us_train['species_id'].map(spec_2_id)
-    assert us_train['species_id'].max()+1 == len(us_train.species_id.unique()), f"map unsuccessful. {us_train['species_id'].max()} vs {len(us_train.species_id.unique())}"
-    # Grab only obs id, species id because lat /lon not necessary at the moment
-    us_train = us_train[['id', 'species_id']]
-    print("labels loaded")
-
-    #grab 20% of labeled data for holdout testing
-    training, test = split_train_test(us_train.to_numpy(), .8)
-    train_ids, train_spec_ids = split_id_specs(training, device)
-    test_ids, test_spec_ids = split_id_specs(test, device)
-    vals = 10 
-    FAKEE = train_ids[:vals] 
-    train_ids = FAKEE 
-    FAKEE2 = test_ids[:vals] 
-    test_ids = FAKEE2 
-    FAKE3 = train_spec_ids[:vals] 
-    train_spec_ids = FAKE3 
-    FAKE4 = test_spec_ids[:vals] 
-    test_spec_ids = FAKE4 
-
-    print("loading images")    
-    tick = time.time()
-    training_imgs = tensor_from_ids(train_ids, f"{paths.GEOCELF_DIR}patches_us/", ARGS.device)
-    test_imgs = tensor_from_ids(test_ids, f"{paths.GEOCELF_DIR}patches_us/", ARGS.device)
-    tock = time.time()
-    diff = tock - tick
-    print(f"images loaded. Took {diff} seconds")
-    
+    train_dataset = Dataset.GEOCELF_Dataset(ARGS.base_dir, 'train',ARGS.country)
+    val_split = .9
+    train_loader = None
+    test_loader = None
+    if ARGS.test:
+        train_samp, test_samp = split_train_test(train_dataset, val_split)
+        train_loader = DataLoader(train_dataset, ARGS.batch_size,  pin_memory=True, num_workers=ARGS.processes, sampler=train_samp) 
+        test_loader = DataLoader(train_dataset, ARGS.batch_size,  pin_memory=True, num_workers=ARGS.processes, sampler=test_samp)
+    else:
+        train_loader = DataLoader(train_dataset, ARGS.batch_size, shuffle=True, pin_memory=True, num_workers=ARGS.processes) 
+        test_dataset = Dataset(ARGS.base_dir, 'test',ARGS.country)
+        test_loader = DataLoader(test_dataset, ARGS.batch_size, shuffle=True, pin_memory=True, num_workers=ARGS.processes)
     # set up net
-    num_channels = training_imgs.shape[1]# num_channels should be idx 1 in the order torch expects
-    num_cats = len(us_train.species_id.unique())
+    num_channels = train_dataset.channels# num_channels should be idx 1 in the order torch expects
+    num_cats = train_dataset.num_cats
     net= cnn.Net(categories=num_cats, num_channels=num_channels)
     loss = torch.nn.CrossEntropyLoss()
     optimizer = optim.Adam(net.parameters(), lr=ARGS.lr)
@@ -138,49 +75,57 @@ def main():
 
     batch_size=ARGS.batch_size
     n_epochs=ARGS.epoch
-    n_minibatches = math.ceil(len(train_ids) / batch_size)
-
-    
+    num_batches = math.ceil(len(train_dataset) / batch_size)
+    print(f"batch size is {batch_size} and size of dataset is {len(train_loader)} total size of dataset is {len(train_dataset)} and num batches is {num_batches}")
+    #n_minibatches = math.ceil(len(train_loader) / batch_size)
+    print("starting training") 
     for epoch in range(n_epochs):
+        tick = time.time()
         net.train()
         loss_meter = []
-        with tqdm(total=(n_minibatches)) as prog:
-            
-            train_bat, label_bat = batch_data(training_imgs, train_spec_ids, batch_size)
-
-        #             assert len(label_bat) == len(train_bat), f"input: {len(label_bat)}, label: {len(train_bat)} batches aren't sized correctly!"
-#             assert label_bat[-1].shape[0] == train_bat[-1].shape[0], "number of training examples and labels don't match!"
-            for batch, labels in zip(train_bat, label_bat):
+        with tqdm(total=len(train_loader), unit="batch") as prog:
+        #import pdb; pdb.set_trace()
+            for i, (labels, batch) in enumerate(train_loader):
+                tickk = time.time()
                 batch = batch.to(device)
                 labels = labels.to(device)                                     
                 # zero the parameter gradients
+                tickkk = time.time()
                 optimizer.zero_grad()
                 # forward + backward + optimize
                 outputs = net(batch.float()) # convert to float so torch happy
-#                 assert outputs.shape[0] == len(labels); f"your logits {outputs.shape} and label {len(labels)} sizes do not match up!"
                 # compute loss
                 loss_rec = loss(outputs, labels) 
                 loss_rec.backward()
                 optimizer.step()
+                tockkk = time.time()
                 # update tqdm
                 prog.update(1)
+                curr_loss = loss_rec.item()
+                #prog.set_description(f"loss: {curr_loss}")
                 # update loss tracker
-                loss_meter.append(loss_rec.item())                                     
-                
+                loss_meter.append(curr_loss)                                     
+                #print(f"training took {diff} sec, just train took {diff2} seconds and dataload took {datload} seconds with {ARGS.processes} workers")
+                tockk = time.time()
+                diff = tockk - tickk
+                difff = tockkk - tickkk
+                ddiff = diff - difff
+                print(f"total time: {diff} time training: {difff} time loading {ddiff}")
+                prog.set_description(f"total time: {diff} time training: {difff} time loading {ddiff} with {ARGS.processes} workers")
         print (f"Average Train Loss: {np.stack(loss_meter).mean(0)}")
-        # test
-
-        #TODO: add batching to eval loop
-        net.eval()
-        curr_test, curr_labels = shuffle_data(test_imgs, test_spec_ids)
-        curr_test = curr_test[:20]
-        test_labels = curr_labels[:20]
-        curr_test = curr_test.to(device)
         
-        outputs = net(curr_test.float()) 
-        accs = topk_acc(outputs, test_labels, topk=(30,1), device=device) # magic no from CELF2020
+        # test
+        net.eval()
+        if ARGS.test:
+            
+            labels, batch = test_loader[randrange(len(test_loader))]
+
+            labels = labels.to(device)
+            batch = batch.to(device)
+        outputs = net(batch.float()) 
+        accs = topk_acc(outputs, labels, topk=(30,1), device=device) # magic no from CELF2020
         print(f"average top 30 accuracy: {accs[0]} average top1 accuracy: {accs[1]}")
-        del outputs
+        del outputs, labels, batch
         
         # save model 
         print(f"saving model for epoch {epoch}")
@@ -192,20 +137,31 @@ def main():
                     'loss': loss_rec,
                     'accuracy': accs
                     }, PATH)
-
-
+        tock = time.time()
+        diff = ( tock-tick)/60
+        print (f"one epoch took {diff} minutes")
+    if not ARGS.test:
+        # TODO: add validation + csv for GeoCLEF here
+        print("GeoCLEF validation not implemented yet!")
 
 if __name__ == "__main__":
     #print(f"torch version: {torch.__version__}") 
     #print(f"numpy version: {np.__version__}")
     parser = argparse.ArgumentParser()
     parser.add_argument("--lr", type=float, help="learning rate of model",required=True)
-    parser.add_argument("--epoch", type=int, help="how many epochs to train the model")
+    parser.add_argument("--epoch", type=int, required=True, help="how many epochs to train the model")
     parser.add_argument("--device", type=int, help="which gpu to send model to, don't put anything to use cpu")
+    parser.add_argument("--processes", type=int, help="how many worker processes to use for data loading", default=1)
     parser.add_argument("--exp_id", type=str, help="experiment id of this run", required=True)
+    parser.add_argument("--country", type=str, help="which country's images to read", default='us')
+    parser.add_argument("--base_dir", type=str, help="what folder to read images from",choices=['DBS_DIR', 'MEMEX_LUSTRE', 'CALC_SCRATCH'], required=True)
     parser.add_argument("--seed", type=int, help="random seed to use")
+    parser.add_argument('--test', dest='test', help="if set, split train into test, val set. If not seif set, split train into test, val set. If not set, train network on full datasett", action='store_true')
     parser.add_argument("--batch_size", type=int, help="size of batches to use", default=256)    
     ARGS, _ = parser.parse_known_args()
+    # parsing which path to use
+    ARGS.base_dir = eval(f"paths.{ARGS.base_dir}")
+    print(f"using base directory {ARGS.base_dir}")
     # Seed
     if ARGS.seed is not None:
         np.random.seed(ARGS.seed)
