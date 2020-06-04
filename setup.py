3b8c3e1233059b9cebeba2e2274da6765849ef05
Lauren Gillespie <leg2015@gmail.com>
2020-06-03 23:29:01 -0700

    removing pycrypto becuase no longer used

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
 	name='deepbiosphere',
 	version='0.0.1',
 	author='moiexpositoalonsolab',
-	install_requires=["numpy", "google-api-python-client", "pyCrypto", "spyder", "jupyter", "earthengine-api", "oauth2client", "pillow", "matplotlib", "torch", "pandas", "tqdm"]
+	install_requires=["numpy", "google-api-python-client", "spyder", "jupyter", "earthengine-api", "oauth2client", "pillow", "matplotlib", "torch", "pandas", "tqdm"]
 	)
