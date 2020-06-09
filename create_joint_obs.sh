d4fffae72f76c511f72058d0f8374352a18fc34b
Lauren Gillespie <leg2015@gmail.com>
2020-06-09 11:19:38 -0700

    adding joint observation creation scripts

---
+++ create_joint_obs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+
+# first, create joint_obs directory
+# need to be in GeoCLEF2020/ directory
+pwd
+mkdir joint_obs
+
+
+CURR_PARAMS=""
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+-base_dir)
+BASE_DIR="$2"
+# CURR_PARAMS=" $CURR_PARAMS $key $2 " # add to list of params to include in Aagos run
+shift # shift past curr argument
+shift # shift past curr value
+;; # indicates end of case
+
+    *)    # unknown option
+    echo "ERROR: unknown command line argument: "$1" not a recognized command"
+    exit 1
+    ;;
+esac
+done
+
+
+
+python3 ../deepbiosphere/scripts/joint_obs.py --country us --base_dir $BASE_DIR
+python3 ../deepbiosphere/scripts/joint_obs.py --country fr --base_dir $BASE_DIR
+cd joint_obs/
+head -1 us_Abbeville_HenryCounty_Alabama.csv > joint_obs_us.csv ; tail -n +2 -q us*.csv >> joint_obs_us.csv
+head -1 fr_Abbeville_DepartementdelaSomme_Picardie.csv > joint_obs_fr.csv ; tail -n +2 -q fr*.csv >> joint_obs_fr.csv
+cp joint_obs_us.csv ../occurrences/
+cp joint_obs_fr.csv ../occurrences/
--- deepbiosphere/scripts/joint_obs.py
+++ deepbiosphere/scripts/joint_obs.py
@@ -55,11 +55,15 @@ def get_joint_from_group(group_df):
 
 def main():
     print("grab data")
-    pth = paths.DBS_DIR
-    us_train_pth = f"{pth}occurrences/occurrences_us_train.csv"
-    us_train = pd.read_csv(us_train_pth, sep=';')
-    filt = pd.read_csv(f"{pth}/occurrences/occurrences_cali_filtered_full.csv")
-    us_train = filt if ARGS.filtered else us_train
+    pth = ARGS.base_dir
+    us_train = None
+    if ARGS.filtered:
+        us_train = pd.read_csv(f"{pth}/occurrences/occurrences_cali_filtered_full.csv")
+    else:
+        us_train_pth = f"{pth}occurrences/occurrences_{ARGS.country}_train.csv"
+        us_train = pd.read_csv(us_train_pth, sep=';')
+        
+
     
     # create a new tuple column
     us_train['lat_lon'] = list(zip(us_train.lat, us_train.lon))
@@ -67,7 +71,7 @@ def main():
     us_latlon = us_train['lat_lon'].tolist()
     # grab location data for the lat lon
     res = rg.search(us_latlon)
-    # grab only the states from the results
+    # grab necessary info from the results
     states = [r['admin1'] for r in res]
     regions = [r['admin2'] for r in res]
     cities = [r['name'] for r in res]
@@ -75,8 +79,7 @@ def main():
     us_train['state'] = states
     us_train['region'] = regions
     us_train['city'] = cities
-    # grab only observations from california
-    # filtered_us = us_train[us_train.state == 'California']
+    # group data into smaller, more manageable chunks
     grouped = us_train.groupby(['city', 'state', 'region'])
     all_datframes = []
     for (grouping, df) in grouped:
@@ -88,19 +91,17 @@ def main():
             city = grouping[0].replace(" ", "")
             region = grouping[2].replace(" ", "")
             state = grouping[1].replace(" ", "")
-            pth_pth = f"{write_pth}{city}_{region}_{state}.csv"
+            pth_pth = f"{write_pth}{ARGS.country}_{city}_{region}_{state}.csv"
 #             print(f"saving to {pth_pth}")
             joint_df.to_csv(pth_pth)
         else:     
             all_datframes.append(joint_df)
-            #     print(f"{joint_df.index.max()}")
-    #     break
-    joint_obs = pd.concat(all_datframes)
-    
-    
-    print("save data")
-    region = 'cali' if ARGS.filtered else 'us'
-    joint_obs.to_csv(f"{pth}/occurrences/joint_obs_{region}.csv")
+
+    if ARGS.filtered:
+        joint_obs = pd.concat(all_datframes)
+        print("save data")
+        region = 'cali' if ARGS.filtered else ARGS.country
+        joint_obs.to_csv(f"{pth}/occurrences/joint_obs_{region}.csv")
     
     
 if __name__ == "__main__":
@@ -108,6 +109,10 @@ if __name__ == "__main__":
     #print(f"numpy version: {np.__version__}")
     print("hello")
     parser = argparse.ArgumentParser()
+    parser.add_argument("--country", type=str, help="which country's images to read", default='us', required=True, choices=['us', 'fr'])
+    parser.add_argument("--base_dir", type=str, help="what folder to read images from",choices=['DBS_DIR', 'MEMEX_LUSTRE', 'CALC_SCRATCH', 'AZURE_DIR'], required=True)
     parser.add_argument('--filtered', dest='filtered', help="if using cali filtered data", action='store_true')
     ARGS, _ = parser.parse_known_args()
+    ARGS.base_dir = eval("paths.{}".format(ARGS.base_dir))
+    print("using base directory {}".format(ARGS.base_dir))
     main()
