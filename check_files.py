71424520650a5144a50f6135576cb4ba41d58eab
Lauren Gillespie <leg2015@gmail.com>
2020-06-17 16:42:28 -0700

    converted fstrings to format

--- check_files.py
+++ check_files.py
@@ -2,37 +2,39 @@ import pandas as pd
 import os
 from os import listdir
 import pprint
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from deepbiosphere.scripts import paths
 import math
+import glob
 import os, os.path
 
 
 
 def main():
-    pth = ARGS.path 
-    us_train_pth = f"{pth}occurrences/occurrences_us_train.csv"
+    pth = ARGS.base_dir 
+    us_train_pth = "{}occurrences/occurrences_us_train.csv".format(pth)
     us_train = pd.read_csv(us_train_pth, sep=';')
-    us_test_pth = f"{pth}occurrences/occurrences_us_test.csv"
+    us_test_pth = "{}occurrences/occurrences_us_test.csv".format(pth)
     us_test = pd.read_csv(us_test_pth, sep=';')#
-    fr_train_pth = f"{pth}occurrences/occurrences_fr_train.csv"
+    fr_train_pth = "{}occurrences/occurrences_fr_train.csv".format(pth)
     fr_train = pd.read_csv(fr_train_pth, sep=';')
-    fr_test_pth = f"{pth}occurrences/occurrences_fr_test.csv"
+    fr_test_pth = "{}occurrences/occurrences_fr_test.csv".format(pth)
     fr_test = pd.read_csv(fr_test_pth, sep=';')#
     # grab all the image files (careful, really slow!)
-    us_dir = f"{pth}patches_us}"
+    us_dir = "({}patches_us)".format(pth)
     #for root, dirs, files in os.walk('python/Lib/email'):
     #print len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
-    paths_us = glob.glob(f"{pth}/patches_us/patches_us_*/*/*/*_alti.npy")
-    paths_fr = glob.glob(f"{pth}/patches_fr/*/*/*_alti.npy")
+    paths_us = glob.glob("{}/patches_us/patches_us_*/*/*/*_alti.npy".format(pth))
+    paths_fr = glob.glob("{}/patches_fr/*/*/*_alti.npy".format(pth))
 
     # how many images are missing?
     us_summed = us_train.shape[0] + us_test.shape[0] 
     fr_summed = fr_train.shape[0]  +fr_test.shape[0]
     us_missed = us_summed - len(paths_us)
     fr_missed = fr_summed - len(paths_fr)
-    print(f"number of us missing files: {us_missed} number of missing fr files: {fr_missed}")
+    print("number of us missing files: {} number of missing fr files: {}".format(us_missed, fr_missed))
 
     # get ids of files that are present
     us_path_ids = [ path.split("_alti")[0].split("/")[-1] for path in us_paths]
@@ -52,12 +54,12 @@ def main():
         abcd = miss % 10000
         ab, cd = math.floor(abcd/100), abcd%100
         cdd = math.ceil((cd+ 1)/5)
-        cdd = f"0{cdd}"  if cdd < 10 else f"{cdd}"
-        ab = f"0{ab}" if miss / 1000 > 1 and ab < 10 else ab
-        cd = f"0{cd}" if miss / 1000 > 1 and cd < 10 else cd
-        subpath = f"patches_us_{cdd}/{cd}/{ab}/"
+        cdd = "0{}".format(cdd)  if cdd < 10 else "{}".format(cdd)
+        ab = "0{}".format(ab) if miss / 1000 > 1 and ab < 10 else ab
+        cd = "0{}".format(cd) if miss / 1000 > 1 and cd < 10 else cd
+        subpath = "patches_us_{}/{}/{}/".format(cdd, cd, ab)
         missing_folders.add(subpath)
-    pprint.pprint(f"missing us folders: {missing_folders}")
+    pprint.pprint("missing us folders: {}".format(missing_folders))
     missing_fr_folders = set()
     for miss in fr_missing:
         abcd = id_ % 10000
@@ -66,34 +68,15 @@ def main():
         cd = "0{}".format(cd) if  cd < 10 else cd
         subpath = "patches_{}/{}/{}/".format(country, cd, ab)
         missing_fr_folders.add(subpath)
-    pprint.pprint(f"missing fr folders: {missing_fr_folders}")
+    pprint.pprint("missing fr folders: {}".format(missing_fr_folders))
 
 if __name__ == "__main__":
     #print(f"torch version: {torch.__version__}") 
     #print(f"numpy version: {np.__version__}")
     parser = argparse.ArgumentParser()
-    parser.add_argument("--lr", type=float, help="learning rate of model",required=True)
-    parser.add_argument("--epoch", type=int, required=True, help="how many epochs to train the model")
-    parser.add_argument("--device", type=int, help="which gpu to send model to, don't put anything to use cpu")
-    parser.add_argument("--processes", type=int, help="how many worker processes to use for data loading", default=1)
-    parser.add_argument("--exp_id", type=str, help="experiment id of this run", required=True)
     parser.add_argument("--base_dir", type=str, help="what folder to read images from",choices=['DBS_DIR', 'MEMEX_LUSTRE', 'CALC_SCRATCH', 'AZURE_DIR'], required=True)
-    parser.add_argument("--country", type=str, help="which country's images to read", default='us', required=True, choices=['us', 'fr', 'both'])
-    parser.add_argument("--seed", type=int, help="random seed to use")
-    parser.add_argument('--test', dest='test', help="if set, split train into test, val set. If not seif set, split train into test, val set. If not set, train network on full dataset", action='store_true')
-    parser.add_argument("--load_size", type=int, help="how many instances to hold in memory at a time", default=1000)
-    parser.add_argument("--batch_size", type=int, help="size of batches to use", default=50)
     ARGS, _ = parser.parse_known_args()
     # parsing which path to use
     ARGS.base_dir = eval("paths.{}".format(ARGS.base_dir))
     print("using base directory {}".format(ARGS.base_dir))
-    # Seed
-    assert ARGS.load_size >= ARGS.batch_size, "load size must be bigger than batch size!"
-    if ARGS.seed is not None:
-        np.random.seed(ARGS.seed)
-        torch.manual_seed(ARGS.seed)
-    if not os.path.exists("{}output/".format(ARGS.base_dir)):
-        os.makedirs("{}output/".format(ARGS.base_dir))
-    if not os.path.exists("{}nets/".format(ARGS.base_dir)):
-        os.makedirs("{}nets/".format(ARGS.base_dir))
     main()
