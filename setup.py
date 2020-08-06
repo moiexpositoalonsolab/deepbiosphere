ca2921466e89b637f3180885959a379b492d26b2
Lauren Gillespie <leg2015@gmail.com>
2020-08-05 22:25:14 -0700

    added dependencies to setup

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
 	name='deepbiosphere',
 	version='0.0.1',
 	author='moiexpositoalonsolab',
-	install_requires=["numpy", "google-api-python-client", "spyder", "jupyter", "earthengine-api", "oauth2client", "pillow", "matplotlib", "torch", "pandas", "tqdm", "reverse_geocoder", "deepdish"]
+	install_requires=["numpy", "google-api-python-client", "spyder", "jupyter", "earthengine-api", "oauth2client", "pillow", "matplotlib", "torch", "pandas", "tqdm", "reverse_geocoder", "deepdish", 'rasterio']
 	)
