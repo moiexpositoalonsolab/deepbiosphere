dbbb806ffba76b055066327d07a2743c1ae26ac8
Lauren Gillespie <lgillesp@login2.cm.cluster>
2022-07-26 15:55:18 -0700

    hacky workaround to require python 3.7

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-import pkg_resources
+import sys
 from setuptools import setup
 import setuptools
 # need the pkg_resources and python_requires to catch older
 # versions of python, setuptools, and pip that don't use PEP 517
 # or newer (aka not checking setup.cfg and pyproject.toml)
-pkg_resources.require(['pip >= 20.0.0', 'setuptools >= 40.6.0'])
+if sys.version_info < (3,7):
+    sys.exit('This package only supports Python 3.7+')
 # bare minimum setup.py so can still editable install packages
 setup(python_requires='>=3.7', packages=setuptools.find_packages(where="./src"), package_dir={"":"src"})
