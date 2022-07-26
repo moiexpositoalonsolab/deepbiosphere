b186dfd7c6a2873ae68729eef7494250fee29a74
Lauren Gillespie <leg2015@gmail.com>
2022-07-26 15:29:38 -0700

    trying to fix versioning errors

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+import pkg_resources
 from setuptools import setup
+import setuptools
+# need the pkg_resources and python_requires to catch older
+# versions of python, setuptools, and pip that don't use PEP 517
+# or newer (aka not checking setup.cfg and pyproject.toml)
+pkg_resources.require(['pip >= 20.0.0', 'setuptools >= 40.6.0'])
 # bare minimum setup.py so can still editable install packages
-setup()
+setup(python_requires='>=3.7', packages=setuptools.find_packages(where="./src"), package_dir={"":"src"})
