cb2e268b49597ce2a56d97dc005d28de5b9ccce7
Lauren Gillespie <leg2015@gmail.com>
2022-08-02 12:10:19 -0700

    Cleaned up scaled BCE loss and added in-house BCE that's faster

--- src/deepbiosphere/Losses.py
+++ src/deepbiosphere/Losses.py
@@ -150,7 +150,6 @@ class AsymmetricLossScaled(nn.Module):
     ''' Notice - optimized version, minimizes memory allocation and gpu uploading,
     favors inplace operations'''
 # standard is gamma-=4, gamma+=1, clip=0.05. gamma > 0 turns on scaling by predicted prob
-# for scaled BCE Experiment: gamma_neg=0, gamma_pos=0, clip=0.0, eps=1e-8, disable_torch_grad_focal_loss=False
     def __init__(self, gamma_neg=4, gamma_pos=1, clip=0.05, eps=1e-8, disable_torch_grad_focal_loss=False):
         super().__init__()
         self.gamma_neg = gamma_neg
@@ -203,23 +202,17 @@ class AsymmetricLossScaled(nn.Module):
 
         return -self.loss.sum()
 
-    
-# from https://github.com/Alibaba-MIIL/ASL/blob/main/src/loss_functions/losses.py
+
 class BCEScaled(nn.Module):
     ''' Notice - optimized version, minimizes memory allocation and gpu uploading,
     favors inplace operations'''
-# standard is gamma-=4, gamma+=1, clip=0.05. gamma > 0 turns on scaling by predicted prob
-# for scaled BCE Experiment: gamma_neg=0, gamma_pos=0, clip=0.0, eps=1e-8, disable_torch_grad_focal_loss=False
-    def __init__(self, gamma_neg=0, gamma_pos=0, clip=0.0, eps=1e-8, disable_torch_grad_focal_loss=False):
+    def __init__(self, eps=1e-8):
+
         super().__init__()
-        self.gamma_neg = gamma_neg
-        self.gamma_pos = gamma_pos
-        self.clip = clip
-        self.disable_torch_grad_focal_loss = disable_torch_grad_focal_loss
         self.eps = eps
 
         # prevent memory allocation and gpu uploading every iteration, and encourages inplace operations
-        self.targets = self.anti_targets = self.xs_pos = self.xs_neg = self.asymmetric_w = self.loss = None
+        self.targets = self.anti_targets = self.xs_pos = self.xs_neg  = self.loss = self.pos_loss = self.neg_loss = None
 
     def forward(self, x, y):
         """"
@@ -236,28 +229,46 @@ class BCEScaled(nn.Module):
         self.xs_pos = torch.sigmoid(x)
         self.xs_neg = 1.0 - self.xs_pos
 
-        # Asymmetric Clipping
-        if self.clip is not None and self.clip > 0:
-            self.xs_neg.add_(self.clip).clamp_(max=1)
-
         # Basic CE calculation
         self.loss = self.targets * torch.log(self.xs_pos.clamp(min=self.eps))
         self.loss.add_(self.anti_targets * torch.log(self.xs_neg.clamp(min=self.eps)))
 
-        # Asymmetric Focusing
-        if self.gamma_neg > 0 or self.gamma_pos > 0:
-            if self.disable_torch_grad_focal_loss:
-                torch._C.set_grad_enabled(False)
-            self.xs_pos = self.xs_pos * self.targets
-            self.xs_neg = self.xs_neg * self.anti_targets
-            self.asymmetric_w = torch.pow(1 - self.xs_pos - self.xs_neg,
-                                          self.gamma_pos * self.targets + self.gamma_neg * self.anti_targets)
-            if self.disable_torch_grad_focal_loss:
-                torch._C.set_grad_enabled(True)
-            self.loss *= self.asymmetric_w
+        # Scaling by present and absent classes
         self.pos_loss = torch.div((self.targets * self.loss).sum(axis=1), (self.targets.sum(axis=1)+self.eps))
         self.neg_loss = torch.div((self.anti_targets * self.loss).sum(axis=1), (self.anti_targets.sum(axis=1)+self.eps))
         self.loss = self.pos_loss + self.neg_loss
 
         return -self.loss.sum()
-    
+
+
+class BCE(nn.Module):
+    ''' Notice - optimized version, minimizes memory allocation and gpu uploading,
+    favors inplace operations'''
+    def __init__(self, eps=1e-8):
+
+        super().__init__()
+        self.eps = eps
+
+        # prevent memory allocation and gpu uploading every iteration, and encourages inplace operations
+        self.targets = self.anti_targets = self.xs_pos = self.xs_neg  = self.loss = None
+
+    def forward(self, x, y):
+        """"
+        Parameters
+        ----------
+        x: input logits
+        y: targets (multi-label binarized vector)
+        """
+
+        self.targets = y
+        self.anti_targets = 1 - y
+
+        # Calculating Probabilities
+        self.xs_pos = torch.sigmoid(x)
+        self.xs_neg = 1.0 - self.xs_pos
+
+        # Basic CE calculation
+        self.loss = self.targets * torch.log(self.xs_pos.clamp(min=self.eps))
+        self.loss.add_(self.anti_targets * torch.log(self.xs_neg.clamp(min=self.eps)))
+
+        return -self.loss.sum()
